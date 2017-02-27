c10bac5b24b946542ff1e84b82d5a487fd747d1c
gordon <gordon@inl.ics.keio.ac.jp>
2017-02-27 13:54:36 +0900

    2nd commit

--- udpcli.c
+++ udpcli.c
@@ -12,53 +12,53 @@
 
 int main()
 {
-	int s, msglen, cnt;
-	socklen_t srvlen, fromlen;
-	char lbuf[80], msg[80];
-	struct sockaddr_in srvskt, from;
+    int s, msglen, cnt;
+    socklen_t srvlen, fromlen;
+    char lbuf[80], msg[80];
+    struct sockaddr_in srvskt, from;
 
-	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
-	}
-	printf("server IP address: ");
-	fgets(lbuf, sizeof lbuf, stdin);
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    printf("server IP address: ");
+    fgets(lbuf, sizeof lbuf, stdin);
 
-	memset(&srvskt, 0, sizeof srvskt);
-	srvskt.sin_family = AF_INET;
-	srvskt.sin_port = htons(SRVPORT);
-	if (inet_aton(lbuf, &srvskt.sin_addr) < 0) {
-		perror("inet_aton");
-		exit(1);
-	}
+    memset(&srvskt, 0, sizeof srvskt);
+    srvskt.sin_family = AF_INET;
+    srvskt.sin_port = htons(SRVPORT);
+    if (inet_aton(lbuf, &srvskt.sin_addr) < 0) {
+        perror("inet_aton");
+        exit(1);
+    }
 
-	for (;;) {
-		printf("message to be sent: ");
-		if (fgets(msg, sizeof msg, stdin) == NULL) {
-			break;
-		}
-		msg[strlen(msg) - 1] = '\0';
-		msglen = strlen(msg);
+    for (;;) {
+        printf("message to be sent: ");
+        if (fgets(msg, sizeof msg, stdin) == NULL) {
+            break;
+        }
+        msg[strlen(msg) - 1] = '\0';
+        msglen = strlen(msg);
 
-		srvlen = sizeof srvskt;
-		if ((cnt = sendto(s, msg, msglen, 0,
-						  (struct sockaddr *)&srvskt, srvlen)) < 0) {
-			perror("sendto");
-			exit(1);
-		}
-		printf("%d bytes sent\n", cnt);
+        srvlen = sizeof srvskt;
+        if ((cnt = sendto(s, msg, msglen, 0,
+                          (struct sockaddr *)&srvskt, srvlen)) < 0) {
+            perror("sendto");
+            exit(1);
+        }
+        printf("%d bytes sent\n", cnt);
 
-		fromlen = sizeof from;
-		if ((cnt = recvfrom(s, msg, sizeof msg, 0,
-							(struct sockaddr *)&from, &fromlen)) < 0) {
-			perror("recvfrom");
-			exit(1);
-		}
-		msg[cnt] = '\0';
-		printf("%d bytes recved: IP %s, port %d, msg %s\n",
-			   cnt, inet_ntoa(from.sin_addr),
-			   ntohs(from.sin_port), msg);
-	}
-	printf("EOF received from keyboard\n");
-	close(s);
+        fromlen = sizeof from;
+        if ((cnt = recvfrom(s, msg, sizeof msg, 0,
+                            (struct sockaddr *)&from, &fromlen)) < 0) {
+            perror("recvfrom");
+            exit(1);
+        }
+        msg[cnt] = '\0';
+        printf("%d bytes recved: IP %s, port %d, msg %s\n",
+               cnt, inet_ntoa(from.sin_addr),
+               ntohs(from.sin_port), msg);
+    }
+    printf("EOF received from keyboard\n");
+    close(s);
 }
--- udpsrv.c
+++ udpsrv.c
@@ -12,51 +12,51 @@
 
 int main()
 {
-	int s, msglen, cnt;
-	socklen_t fromlen;
-	in_port_t myport;
-	char msg[80];
-	struct sockaddr_in myskt, from;
+    int s, msglen, cnt;
+    socklen_t fromlen;
+    in_port_t myport;
+    char msg[80];
+    struct sockaddr_in myskt, from;
 
-	myport = SRVPORT;
+    myport = SRVPORT;
 
-	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
-	}
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
 
-	memset(&myskt, 0, sizeof myskt);
-	myskt.sin_family = AF_INET;
-	myskt.sin_port = htons(myport);
-	myskt.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(s, (struct sockaddr *)&myskt, sizeof myskt) < 0){
-		perror("bind");
-		exit(1);
-	}
+    memset(&myskt, 0, sizeof myskt);
+    myskt.sin_family = AF_INET;
+    myskt.sin_port = htons(myport);
+    myskt.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(s, (struct sockaddr *)&myskt, sizeof myskt) < 0){
+        perror("bind");
+        exit(1);
+    }
 
-	for (;;) {
-		fromlen = sizeof from;
-		if ((cnt = recvfrom(s, msg, sizeof msg, 0,
-							(struct sockaddr *)&from, &fromlen)) < 0) {
-			perror("recvfrom");
-			exit(1);
-		}
-		msg[cnt] = '\0';
-		msglen = strlen(msg);
-		if (!strcmp(msg, "exit")) {
-			break;
-		}
-		printf("%d bytes recved: IP %s, port %d, msg %s\n",
-			   cnt, inet_ntoa(from.sin_addr),
-			   ntohs(from.sin_port), msg);
+    for (;;) {
+        fromlen = sizeof from;
+        if ((cnt = recvfrom(s, msg, sizeof msg, 0,
+                            (struct sockaddr *)&from, &fromlen)) < 0) {
+            perror("recvfrom");
+            exit(1);
+        }
+        msg[cnt] = '\0';
+        msglen = strlen(msg);
+        if (!strcmp(msg, "exit")) {
+            break;
+        }
+        printf("%d bytes recved: IP %s, port %d, msg %s\n",
+               cnt, inet_ntoa(from.sin_addr),
+               ntohs(from.sin_port), msg);
 
-		if ((cnt = sendto(s, msg, msglen, 0,
-						  (struct sockaddr *)&from, fromlen)) < 0) {
-			perror("sendto");
-			exit(1);
-		}
-		printf("%d bytes sent\n", cnt);
-	}
-	printf("EOF received from keyboard\n");
-	close(s);
+        if ((cnt = sendto(s, msg, msglen, 0,
+                          (struct sockaddr *)&from, fromlen)) < 0) {
+            perror("sendto");
+            exit(1);
+        }
+        printf("%d bytes sent\n", cnt);
+    }
+    printf("EOF received from keyboard\n");
+    close(s);
 }
